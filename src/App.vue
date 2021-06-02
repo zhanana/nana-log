d712fcd1c4eeceb6d31bf331116bb546b9318d97
zhanana <15967137550@163.com>
2021-06-02 11:14:59 +0800

    创建404页面

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <router-view/>
-    <hr/>
     </div>
 </template>
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div>肚子空空，请检查网址是否正确！</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'',
+    components: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
