3c64d7585d38e2081251131a55a46e481aa7e496
zhanana <15967137550@163.com>
2021-06-03 20:01:40 +0800

    利用svgo-loader删除svg自带的fill属性问题

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,8 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end()  // 包含icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
-      // .use('svgo-loader').loader('svgo-loader')
-      // .tap(options=>({...options, plugins:[{removeAttrs:{attrs:'fill'}}]})).end()  // 去除svg自带颜色的影响
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options=>({...options,plugin:[{removeAttrs:{attrs:'fill'}}]})).end()// 去除svg自带颜色的影响
       //   .tap(options=>({     // 这是另一种尝试
       //     ...options,
       //     plugins: [
