d4bc0f0ed36984f195b0f92cfebdc7943559914b
zhanana <15967137550@163.com>
2021-06-05 15:31:01 +0800

    使用TS实现一个组件

--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -1,55 +1,69 @@
 <template>
   <ul class="types">
-    <li :class="type === '-'&&'selected'" @click="selectType('-')">支出</li>
-    <li :class="type === '+'&&'selected'" @click="selectType('+')">收入</li>
+    <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+    <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
   </ul>
 </template>
 
-<script>
-export default {
-  name: "Types",
-  props:['xxx'],
-  data(){
-    return {
-      type:'-'//'-表示支出，+表示收入
-    }
-  },
-  mounted(){
-    console.log(this.xxx)
-  },
-  methods:{
-    selectType(type){
-      if(type !== '-' && type !== '+'){
-        throw new Error('type is unknown')
-      }
-      this.type = type
-    }
-  }
-};
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+@Component
+export default class Types extends Vue{
+  type = '-';
+  selectType(type:string){
+       if(type !== '-' && type !== '+'){
+         throw new Error('type is unknown')
+       }
+       this.type = type
+     }
+}
+
+// export default {
+//   name: "Types",
+//   props:['xxx'],
+//   data(){
+//     return {
+//       type:'-'//'-表示支出，+表示收入
+//     }
+//   },
+//   mounted(){
+//     console.log(this.xxx)
+//   },
+//   methods:{
+//     selectType(type){
+//       if(type !== '-' && type !== '+'){
+//         throw new Error('type is unknown')
+//       }
+//       this.type = type
+//     }
+//   }
+// };
 </script>
 
 <style scoped lang="scss">
-.types{
-    background: #c4c4c4;
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
     display: flex;
-    text-align: center;
-    font-size: 24px;
-    > li{
-        width: 50%;
-        height: 64px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        &.selected::after{//支出下面的横线设置样式
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 4px;
-            background: #333;
-        }
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      //支出下面的横线设置样式
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
     }
+  }
 }
 </style>
