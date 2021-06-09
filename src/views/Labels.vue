b788ae3781fc95df80f46d1a007033121adeb43c
zhanana <15967137550@163.com>
2021-06-09 19:53:41 +0800

    label页面样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,20 +1,65 @@
 <template>
-    <div>
-        <Layout>
-        <p>labels.vue</p>
-        </Layout>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right" />
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
     </div>
+  </Layout>
 </template>
 
 <script lang="ts">
 export default {
-    name:'Labels',
-    components: {
-
-    },
+  name: "Labels",
+  components: {},
 };
 </script>
 
-<style scoped >
-
+<style scoped lang="scss">
+    .tags{
+        background: white;
+        font-size: 16px;
+        padding: 16px;
+        > li{
+            min-height: 44px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom:1px solid #e6e6e6;
+            svg{
+                width: 18px;
+                height: 18px;
+                color: #666;
+                margin-right: 16px;
+            }
+        }
+    }
+    .createTag{
+        background: #767676;
+        color: white;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+        &-wrapper{
+            text-align: center;
+            padding: 16px;
+        }
+    }
 </style>
