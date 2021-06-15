1eafd404e23af31dc343acc555c9a1e00d5e8331
zhanana <15967137550@163.com>
2021-06-15 14:44:19 +0800

    将tag的string改为{id,name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,18 @@
+<template>
+    <layout>
+        编辑标签
+    </layout>
+</template>
+
+<script lang="ts">
+import Layout from '@/components/Layout.vue';
+export default {
+  components: { Layout },
+    name:'EditLabel',
+
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
