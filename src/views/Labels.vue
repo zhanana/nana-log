5b7adbb7a43393c62c910db8bf3460c2e2a47a3d
zhanana <15967137550@163.com>
2021-06-23 22:04:05 +0800

    抽离TagHelper

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -16,6 +16,9 @@
 import Vue from "vue"
 import {Component} from "vue-property-decorator";
 import Button from '@/components/Button.vue';
+import { mixins } from "vue-class-component";
+import {TagHelper as tagHelper} from '@/mixins/TagHelper'
+
 @Component({
   components: {
     Button,
@@ -26,16 +29,9 @@ import Button from '@/components/Button.vue';
     }
   },
 })
-export default class Labels extends Vue{
+export default class Labels extends mixins(tagHelper){
   beforeCreate(){
     this.$store.commit('fetchTags');
-  }
-  createTag(){
-     const name = window.prompt("请输入标签");
-    if(!name){
-      return window.alert('标签名不能为空');
-    }
-    this.$store.commit('createTag',name);
   }
 }
 </script>
