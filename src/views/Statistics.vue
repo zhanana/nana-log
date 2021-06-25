dcc6f57bbc878c6e570b9b72e03d7387bdd19efb
zhanana <15967137550@163.com>
2021-06-25 15:54:36 +0800

    日期选择

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,30 +1,38 @@
 <template>
     <Layout>
          <Types class-prefix="xxx" :value.sync="yyy"/>
+         <Tabs :data-source="array" :value.sync = "hhh"/>
     </Layout>
 </template>
 
 <script lang="ts">
 import Types from '@/components/money/Types.vue';
+import Tabs from '@/components/Tabs.vue'
 import Vue from 'vue';
 import { Component } from "vue-property-decorator"
 @Component({
     components: {
         Types,
+        Tabs
     },
 })
 export default class Statistics extends Vue{
-    yyy = '-'
+    yyy = '-';
+    hhh = 'day';
+    array = [
+        {text:'按天',value:'day'},
+        {text:'按周',value:'week'},
+        {text:'按月',value:'month'},]
 
 }
 </script>
 
 <style scoped lang="scss">
-    .xxx ::v-deep li{
+    ::v-deep .xxx-item{
         background: white;
-        &.selected{
+        &.selected {
             background: #c4c4c4;
-            &::after{
+            &::after {
                 display: none;
             }
         }
