bec2881ba8a6c1dc5640ad02887d4ca28cbc892d
zhanana <15967137550@163.com>
2021-06-25 21:34:14 +0800

    更新css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,44 +1,53 @@
 <template>
-    <Layout>
-         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync = "type"/>
-         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync = "interval"/>
-         <div>
-             type:{{type}}
-             <br/>
-             interval:{{interval}}
-         </div>
-    </Layout>
+  <Layout>
+    <Tabs
+      class-prefix="type"
+      :data-source="recordTypeList"
+      :value.sync="type"
+    />
+    <Tabs
+      class-prefix="interval"
+      :data-source="intervalList"
+      :value.sync="interval"
+    />
+    <div>
+      type:{{ type }}
+      <br />
+      interval:{{ interval }}
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
-import Tabs from '@/components/Tabs.vue';
-import Vue from 'vue';
+import Tabs from "@/components/Tabs.vue";
+import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import intervalList from '@/constant/intervalList';
-import recordTypeList from '@/constant/recordTypeList';
+import intervalList from "@/constant/intervalList";
+import recordTypeList from "@/constant/recordTypeList";
 @Component({
-    components: {
-        Tabs
-    },
+  components: {
+    Tabs,
+  },
 })
-export default class Statistics extends Vue{
-    type = '-';
-    interval = 'day';
-    intervalList = intervalList;
-    recordTypeList = recordTypeList;
-
+export default class Statistics extends Vue {
+  type = "-";
+  interval = "day";
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
 }
 </script>
 
 <style scoped lang="scss">
-    ::v-deep .type-tabs-item{
-        background: white;
-        &.selected {
-            background: #c4c4c4;
-            &::after {
-                display: none;
-            }
-        }
+::v-deep .type-tabs-item {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      display: none;
     }
-
+  }
+}
+::v-deep .interval-tabs-item{
+    height: 48px;
+}
 </style>
