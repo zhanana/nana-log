9836fb3dca75b0c374292ead0a518fc1e49eeb6f
zhanana <15967137550@163.com>
2021-06-26 17:36:21 +0800

    列表展示完成

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,14 +5,9 @@
       :data-source="recordTypeList"
       :value.sync="type"
     />
-    <Tabs
-      class-prefix="interval"
-      :data-source="intervalList"
-      :value.sync="interval"
-    />
     <ol>
-      <li v-for="(group, index) in result" :key="index.id">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group,index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }} <span>￥{{group.total}}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
             <span>{{ tagString(item.tags) }}</span>
@@ -31,6 +26,9 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import intervalList from "@/constant/intervalList";
 import recordTypeList from "@/constant/recordTypeList";
+import dayjs from 'dayjs';
+import clone from "@/lib/clone";
+
 type RootState = {
   recordList: RecordItem[];
   createRecordError: Error | null;
@@ -45,7 +43,6 @@ type RecordItem = {
   amount: number; // 数据类型 object | string
   createAt?: string; // 类 / 构造函数
 };
-type HashTableValue = { title: string; items: RecordItem[] };
 type Tag = {
   id: string;
   name: string;
@@ -60,29 +57,52 @@ export default class Statistics extends Vue {
     return tags.length === 0 ? "无" : tags.map((t) => t.name).join(",");
   }
 
+beautify(string: string){
+  const day = dayjs(string);
+  const now = dayjs();
+  if(day.isSame(now,'day')){
+    return '今天';
+  } else if(day.isSame(now.subtract(1,'day'),'day')){
+    return '昨天';
+  } else if(day.isSame(now.subtract(2,'day'),'day')){
+    return '前天';
+  } else if(day.isSame(now,'year')){
+    return day.format('M月D日');
+  } else {
+    return day.format('YYYY年M月D日');
+  }
+}
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
-  get result() {
+  get groupedList() {
     const { recordList } = this;
-    const hashTable: { [key: string]: HashTableValue } = {}; //如何申明一个空对象的类型
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createAt!.split("T");
-      hashTable[date] = hashTable[date] || { title: date, items: [] };
-
-      hashTable[date].items.push(recordList[i]);
+    if(recordList.length===0){return [];}
+    const newList = clone(recordList)
+    .filter(r => r.type === this.type)
+    .sort((a,b)=>dayjs(b.createAt).valueOf()-dayjs(a.createAt).valueOf());
+    type Result = {title:string,total?:number,items:RecordItem[]}[];
+    const result:Result = [{title:dayjs(newList[0].createAt).format('YYYY-MM-DD'),items:[newList[0]]}];
+    for(let i = 1; i<newList.length; i++){
+      const current = newList[i];
+      const last = result[result.length-1];
+      if(dayjs(last.title).isSame(dayjs(current.createAt),'day')){
+        last.items.push(current)
+      } else {
+        result.push({title:dayjs(current.createAt).format('YYYY-MM-DD'),items:[current]});
+      }
     }
-    console.log(hashTable);
-    return hashTable;
+    result.map(group=>{
+      group.total = group.items.reduce((sum,item) =>{return sum + item.amount} ,0);
+    });   
+    return result;
   }
   beforeCreate() {
     this.$store.commit("fetchRecords");
   }
 
   type = "-";
-  interval = "day";
-  intervalList = intervalList;
   recordTypeList = recordTypeList;
 }
 </script>
@@ -90,9 +110,9 @@ export default class Statistics extends Vue {
 <style scoped lang="scss">
 ::v-deep {
   .type-tabs-item {
-    background: white;
-    &.selected {
       background: #c4c4c4;
+    &.selected {
+    background: white;
       &::after {
         display: none;
       }
